85ab5cd3e8d8b00c946762b1b5c2cca3232b5a81
santhosh <ssanthoshss98@gmail.com>
2022-09-09 15:24:15 +0530

    added dummy condition

--- detection.py
+++ detection.py
@@ -8,6 +8,8 @@ while True:
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    if len(faces):
+        print("Face Detected")
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
